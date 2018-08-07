f8eb065e3903ba728b5857234515e04d3c52ab4d
restot <92ilya.icom@gmail.com>
2018-08-07 11:15:19 -0400

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -2,7 +2,7 @@
 echo $(date +%Y-%m-%d\ %T\ %Z) --acrhive >> cronlog.log
 sudo systemctl stop s_rails
 cd /home/restot/archive_logs
-tar -cvvf $(date +%Y-%m-%d).tar /home/restot/runtime/ecc/log
+tar -czvf $(date +%Y-%m-%d).tar /home/restot/runtime/ecc/log
 cd /home/restot/runtime/ecc/log
 sudo rm -rfv *
 sudo systemctl start s_rails
